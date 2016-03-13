21c78a2461e6b28bf46e8eb9b063422e11fea1d7
Terry Chen <terryschen@gmail.com>
2016-03-12 22:35:36 -0500

    add vec capability

--- tsc_lib/src/main.c
+++ tsc_lib/src/main.c
@@ -3,6 +3,9 @@
 #define TSC_DEFINE
 #include "tsc.h"
 
+tsc_vec_define(vi, int)
+typedef tsc_vec_type(vi) vec_int_t;
+
 int main(int argc, const char ** argv) {
   (void)argc;
   (void)argv;
@@ -91,10 +94,64 @@ int main(int argc, const char ** argv) {
   assert(tsc_strstartswith("abcd123", "ab"));
   assert(!tsc_strstartswith("abcd123", "bb"));
   
+  vec_int_t a;
+  tsc_vec_init(a);
+  tsc_vec_push(vi, a, 0);
+  tsc_vec_push(vi, a, 1);
+  tsc_vec_push(vi, a, 2);
+  tsc_vec_push(vi, a, 3);
+  tsc_vec_push(vi, a, 4);
   
+  vec_int_t b;
+  tsc_vec_init(b);
+  tsc_vec_push(vi, b, 10);
+  tsc_vec_push(vi, b, 20);
+  tsc_vec_push(vi, b, 30);
+  tsc_vec_push(vi, b, 40);
   
+  printf("first: %d\n", tsc_vec_first(a));
+  printf("last:  %d\n", tsc_vec_last(a));
   
+  size_t i = 0;
+  int v;
+  tsc_vec_foreach(a, v, i)
+    printf("v[%zu] = %d\n", i, v);
+  int * vp;
+  tsc_vec_foreach_ptr(a, vp, i)
+    *vp = i * *vp;
+  tsc_vec_foreach_rev(a, v, i)
+    printf("v[%zu] = %d\n", i, v);
+  tsc_vec_foreach_ptr_rev(a, vp, i) {
+    *vp = *vp + i;
+    printf("v[%zu] = %d\n", i, *vp);
+  }
   
+  printf("\n\n");
+  
+  tsc_vec_foreach(b, v, i)
+    printf("v[%zu] = %d\n", i, v);
+  printf("\n\n");
+  tsc_vec_extend(vi, b, a);
+
+  tsc_vec_foreach(b, v, i)
+    printf("v[%zu] = %d\n", i, v);  
+  printf("\n\n");
+  tsc_vec_pop(b);
+  tsc_vec_foreach(b, v, i)
+    printf("v[%zu] = %d\n", i, v);    
+  printf("\n\n");
+  
+  tsc_vec_reverse(vi, b);
+  tsc_vec_foreach(b, v, i)
+    printf("v[%zu] = %d\n", i, v);
+
+  printf("\n\n");  
+  tsc_vec_splice(b, 2, 2);
+  tsc_vec_foreach(b, v, i)
+    printf("v[%zu] = %d\n", i, v);
+    
+  tsc_vec_destroy(a);
+  tsc_vec_destroy(b);
   
   return 0;
 }
--- tsc_lib/src/tsc.h
+++ tsc_lib/src/tsc.h
@@ -12,9 +12,124 @@
   #define TSC_EXTERN extern
 #endif
 
+#define tsc_roundup32(x) (--(x), (x)|=(x)>>1, (x)|=(x)>>2, (x)|=(x)>>4, (x)|=(x)>>8, (x)|=(x)>>16, ++(x))
+
+// CAUTION, if OOM happens in resize / copy / push, you are responsible for cleaning up dst
+// Not possible to do it here b/c we don't have info on the resources pointed by a[i]
+
+#define tsc_vec_resize(name, v, s) tsc_vec_##name##_resize(&(v),(s))
+#define tsc_vec_copy(name, d, s) tsc_vec_##name##_copy(&(d),&(s))
+#define tsc_vec_push(name, v, x) tsc_vec_##name##_push(&(v), (x))
+#define tsc_vec_compact(name, v) tsc_vec_##name##_resize(&(v), (v).n)
+#define tsc_vec_extend(name, d, s) tsc_vec_##name##_extend(&(d),&(s))
+#define tsc_vec_reverse(name, v) tsc_vec_##name##_reverse(&(v))
+#define tsc_vec_subvec(name, d, s, start, count) tsc_vec_##name##_subvec(&(d), &(s), start, count)
+
+#define tsc_vec_init(v) ((v).n = (v).m = 0, (v).a = 0)
+#define tsc_vec_destroy(v) free((v).a)
+#define tsc_vec_A(v, i) ((v).a[(i)])
+#define tsc_vec_pop(v) ((v).a[--(v).n])
+#define tsc_vec_size(v) ((v).n)
+#define tsc_vec_max(v) ((v).m)
+#define tsc_vec_ptr(v) ((v).a)
+#define tsc_vec_clear(v) ((v).n = 0)
+#define tsc_vec_first(v) ((v).a[0])
+#define tsc_vec_last(v) ((v).a[(v).n-1])
+#define tsc_vec_type(name) tsc_vec_##name##_t
+#define tsc_vec_define(name, type)                                                      \
+  typedef struct { size_t n, m; type *a; } tsc_vec_##name##_t;                          \
+  static inline const char* tsc_vec_##name##_resize(tsc_vec_##name##_t *v, size_t s) {  \
+    type *tmp;                                                                          \
+    if( (tmp = (type*)realloc(v->a, sizeof(type) * s)) == NULL )                        \
+      return "OOM";                                                                     \
+    v->m = s; v->a = tmp; return NULL;                                                  \
+  }                                                                                     \
+  static inline const char*                                                             \
+  tsc_vec_##name##_copy(tsc_vec_##name##_t *dst, tsc_vec_##name##_t *src) {             \
+    const char *estr;                                                                   \
+    if (dst->m < src->n)                                                                \
+      if( (estr = tsc_vec_##name##_resize(dst, src->n)) != NULL )                       \
+        return estr;                                                                    \
+    dst->n = src->n;                                                                    \
+    memcpy(dst->a, src->a, sizeof(type) * src->n); return NULL;                         \
+  }                                                                                     \
+  static inline const char* tsc_vec_##name##_push(tsc_vec_##name##_t *v, type x) {      \
+    const char *estr;                                                                   \
+    if(v->n == v->m) {                                                                  \
+      size_t m = v->m ? v->m << 1 : 4;                                                  \
+      if( (estr = tsc_vec_##name##_resize(v, m)) != NULL )                              \
+        return estr;                                                                    \
+    }                                                                                   \
+    v->a[v->n++]= x; return NULL;                                                       \
+  }                                                                                     \
+  static inline const char*                                                             \
+  tsc_vec_##name##_extend(tsc_vec_##name##_t *dst, tsc_vec_##name##_t *src) {           \
+    const char *estr;                                                                   \
+    size_t n = dst->n + src->n;                                                         \
+    if(dst->m < n)                                                                      \
+      if( (estr = tsc_vec_##name##_resize(dst, n)) != NULL )                            \
+        return estr;                                                                    \
+    memcpy(dst->a + dst->n, src->a, sizeof(type) * src->n);                             \
+    dst->n = n; return NULL;                                                            \
+  }                                                                                     \
+  static inline const char * tsc_vec_##name##_reverse(tsc_vec_##name##_t *v) {          \
+    type tmp;                                                                           \
+    for( size_t i = (v->n-1) >> 1 ; (i + 1) > 0 ; --i) {                                \
+      tmp = v->a[i]; v->a[i] = v->a[v->n - i - 1]; v->a[v->n - i - 1] = tmp;            \
+    }                                                                                   \
+    return NULL;                                                                        \
+  }                                                                                     \
+  static inline const char* tsc_vec_##name##_subvec(tsc_vec_##name##_t *dst,            \
+    tsc_vec_##name##_t *src, size_t start, size_t count)                                \
+  {                                                                                     \
+    const char *estr;                                                                   \
+    size_t n;                                                                           \
+    if(start > src->n) {                                                                \
+      dst->n = 0;                                                                       \
+      return NULL;                                                                      \
+    }                                                                                   \
+    if(start + count > src->n) {                                                        \
+      n = src->n - start;                                                               \
+    } else {                                                                            \
+      n = count;                                                                        \
+    }                                                                                   \
+    if( (estr = tsc_vec_##name##_resize(dst, n)) != NULL )                              \
+      return estr;                                                                      \
+    memmove(dst->a, src->a + start, sizeof(type) * n);                                  \
+    dst->n = n; return NULL;                                                            \
+  }
+#define tsc_vec_splice(v, start, count) do {                                            \
+    if(count == 0) break;                                                               \
+    if(start > (v).n) {                                                                 \
+      (v).n = 0;                                                                        \
+      break;                                                                            \
+    }                                                                                   \
+    if(start + count > (v).n) {                                                         \
+      (v).n = start;                                                                    \
+      break;                                                                            \
+    }                                                                                   \
+    memmove((v).a + start, (v).a + start + count,                                       \
+      sizeof((v).a[0]) * ((v).n - start - count));                                      \
+    (v).n -= count;                                                                     \
+  } while(0)
+#define tsc_vec_foreach(v, var, iter)                                                   \
+  if( (v).n > 0 )                                                                       \
+    for ( (iter) = 0; ((iter) < (v).n) && (((var) = (v).a[(iter)]), 1); ++(iter))
+#define tsc_vec_foreach_rev(v, var, iter)                                               \
+  if( (v).n > 0 )                                                                       \
+    for ( (iter) = v.n - 1 ; ((iter + 1) > 0) && (((var) = (v).a[(iter)]), 1); --(iter))
+#define tsc_vec_foreach_ptr(v, var, iter)                                               \
+  if( (v).n > 0 )                                                                       \
+    for ( (iter) = 0; ((iter) < (v).n) && (((var) = &((v).a[(iter)])), 1); ++(iter))
+#define tsc_vec_foreach_ptr_rev(v, var, iter)                                           \
+  if( (v).n > 0 )                                                                       \
+    for ( (iter) = v.n - 1 ; ((iter + 1) > 0) && (((var) = &((v).a[(iter)]) ), 1); --(iter))
+
+
 /// helper that never return error
 
 TSC_EXTERN int    tsc_strstartswith(const char *s, const char *start);
+TSC_EXTERN int    tsc_strendswith(const char *s, const char *end);
 TSC_EXTERN char * tsc_strtrimleft_inplace(char *s);
 TSC_EXTERN char * tsc_strtrimright_inplace(char *s);
 TSC_EXTERN char * tsc_strtrim_inplace(char *s);
@@ -26,6 +141,7 @@ TSC_EXTERN const char * tsc_strtrim(char **ret, const char *s);
 TSC_EXTERN const char * tsc_strdup(char **ret, const char *s);
 TSC_EXTERN const char * tsc_strndup(char **ret, const char *s, size_t n);
 TSC_EXTERN const char * tsc_strflatten(char **ret, char ** str_array, size_t n, const char * sep, size_t sep_sz);
+TSC_EXTERN inline const char * tsc_strtrunc(char **ret, const char *s, size_t n);
 
 /// files
 
@@ -45,6 +161,18 @@ int tsc_strstartswith(const char *s, const char *start) {
       return 1;
     else if(*s != *start) 
       return 0;
+}
+
+int tsc_strendswith(const char *s, const char *end) {
+  int end_len;
+  int str_len;
+  
+  if (NULL == s || NULL == end) return 0;
+
+  end_len = strlen(end);
+  str_len = strlen(s);
+
+  return str_len < end_len ? 1 : !strcmp(s + str_len - end_len, end);
 }
 
 char* tsc_strtrimleft_inplace(char *str) {
@@ -101,6 +229,12 @@ const char * tsc_strflatten(char **ret, char ** str_array, size_t n, const char 
   return NULL;
 }
 
+
+inline const char * tsc_strtrunc(char **ret, const char *s, size_t n) {
+  return tsc_strndup(ret, s, n);
+}
+
+
 const char * tsc_strtrim(char **ret, const char *s) {
   const char *end;
   *ret = NULL;
@@ -113,9 +247,8 @@ const char * tsc_strtrim(char **ret, const char *s) {
 
   // rtrim
   end = s + strlen(s) - 1;
-  while (end > s && isspace(*end)) {
+  while (end > s && isspace(*end))
     end--;
-  }
   
   return tsc_strndup(ret, s, end - s + 1);
 }
@@ -134,7 +267,7 @@ const char * tsc_strndup(char **ret, const char *s, size_t n) {
   if( (*ret = (char *) malloc(n+1)) == NULL )
     return "OOM";
   strncpy(*ret, s, n);  // use strncpy to ensure *ret is properly padded with zero
-  (*ret)[n] = 0;
+  (*ret)[n] = '\0';
   return NULL;
 }
 
@@ -157,9 +290,7 @@ const char * tsc_freadline(char **ret, FILE *stream) {
   while(1) {
     if( fgets(*ret + n, avail - n, stream) == NULL ) {
       if(ferror(stream) != 0) {
-        free(*ret);
-        *ret = NULL;
-        return "FGETS FAILED";
+        free(*ret); *ret = NULL; return "FGETS FAILED";
       } else {                  // normal condition
         return "EOF";
       }
@@ -174,9 +305,7 @@ const char * tsc_freadline(char **ret, FILE *stream) {
     if( feof(stream) != 0 ) {
       if(nlast == (avail - 2)) {  // bad luck, eof at boundary of buffer (need to grow it by 1)
         if( (tmp = (char *) realloc(*ret, ++avail)) == NULL ) {
-          free(*ret);
-          *ret = NULL;
-          return "OOM";
+          free(*ret); *ret = NULL; return "OOM";
         }
       }
       return "EOF";
@@ -186,9 +315,7 @@ const char * tsc_freadline(char **ret, FILE *stream) {
       n     = avail - 1;        // avail - 1 b/c we want to reuse the old \0 spot
       avail = avail << 1;
       if( (tmp = (char *) realloc(*ret, avail)) == NULL ) {
-        free(*ret);
-        *ret = NULL;
-        return "OOM";
+        free(*ret); *ret = NULL; return "OOM";
       }
       *ret = tmp;
     }
@@ -220,9 +347,7 @@ const char * tsc_freadlines(char ***ret, int *nlines, FILE *stream) {
         if( (tmp = (char **) realloc(*ret, avail << 1)) == NULL ) {
           for(int i = 0 ; i < avail ; i++) 
             free((*ret)[i]);
-          free((*ret));
-          *ret = NULL;
-          return "OOM";
+          free((*ret)); *ret = NULL; return "OOM";
         }
         *ret = tmp;
         avail = avail << 1;
@@ -253,10 +378,8 @@ const char * tsc_freadline0(char **ret, int *sz, FILE *stream) {
   while(1) {
     if( fgets(*ret + n, avail - n, stream) == NULL ) {
       if(ferror(stream) != 0) {
-        free(*ret);
-        *ret = NULL;
         if(sz) *sz = 0;
-        return "FGETS FAILED";
+        free(*ret); *ret = NULL; return "FGETS FAILED";
       } else {                  // normal condition
         if(sz) *sz = n;
         return "EOF";
@@ -277,10 +400,8 @@ const char * tsc_freadline0(char **ret, int *sz, FILE *stream) {
     if( feof(stream) != 0 ) {
       if(nlast == (avail - 2)) {  // bad luck, eof at boundary of buffer (need to grow it by 1)
         if( (tmp = (char *) realloc(*ret, ++avail)) == NULL ) {
-          free(*ret);
-          *ret = NULL;
-          if(sz) *sz = 0;
-          return "OOM";
+          if(sz) *sz = 0; 
+          free(*ret); *ret = NULL; return "OOM";
         }
       }
       if(sz) *sz = nlast+1;
@@ -291,10 +412,8 @@ const char * tsc_freadline0(char **ret, int *sz, FILE *stream) {
       n     = avail - 1;        // avail - 1 b/c we want to reuse the old \0 spot
       avail = avail << 1;
       if( (tmp = (char *) realloc(*ret, avail)) == NULL ) {
-        free(*ret);
-        *ret = NULL;
         if(sz) *sz = 0;
-        return "OOM";
+        free(*ret); *ret = NULL; return "OOM";
       }
       *ret = tmp;
       memset(*ret + n, 0, avail - n);
