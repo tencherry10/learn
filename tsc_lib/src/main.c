0b2825c3f46ad051bd4cd0cfd332dd2b47822ed8
Terry Chen <terry.chen@analog.com>
2016-03-18 15:01:28 -0400

    base64 enc/dec

--- tsc_lib/src/main.c
+++ tsc_lib/src/main.c
@@ -499,7 +499,7 @@ int main(int argc, const char ** argv) {
     free(mat3d);
   }
   
-  {
+  if(0) {
     char *user, *pass;
     const char *estr;
     printf("user: "); assert(tsc_freadline(&user, stdin) == NULL);  tsc_strtrim_inplace(user);
@@ -510,6 +510,40 @@ int main(int argc, const char ** argv) {
     
     free(user);
     free(pass);
+  }
+  
+  {
+    char *enc, *dec; 
+    size_t sz;
+    char *test1 = "Man is distinguished, not only by his reason, " \
+      "but by this singular passion from other animals, which is a lust of the mind, "  \
+      "that by a perseverance of delight in the continued and indefatigable generation of knowledge, " \
+      "exceeds the short vehemence of any carnal pleasure.";
+    char *res1  = "TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlz"  \
+                  "IHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2Yg"  \
+                  "dGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGlu"  \
+                  "dWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRo"  \
+                  "ZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4=";
+    
+    printf("orig str:\n%s\n",test1);
+    
+    assert(tsc_base64_enc(&enc, test1, strlen(test1))==NULL);
+    
+    printf("enc str:\n%s\n",enc);
+    
+    assert(strcmp(enc,res1) == 0);
+    
+    //~ enc[3] = '.';
+    
+    printf("dec estr: %s\n", tsc_base64_dec(&dec, &sz, enc, strlen(enc)));
+    
+    if(dec) {
+      printf("dec str:\n%s\n",dec);
+      assert(strcmp(test1,dec) == 0);
+    }
+    
+    free(dec);
+    free(enc);
   }
   
   return 0;
